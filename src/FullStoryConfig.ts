3ff7e84233063720888a714ce3742869eddf3760
Ryan Wang <ryanwang@fullstory.com>
2023-01-13 12:04:53 -0500

    set up allowlistTrackEvents

--- src/FullStoryConfig.ts
+++ src/FullStoryConfig.ts
@@ -9,14 +9,20 @@ import { FSSuffixedProperties } from './utils/FSSuffixedProperties';
 
 interface FullStoryPluginConfig {
   enableFSSessionUrlInEvents?: boolean;
+  allowlistAllTrackEvents?: boolean;
+  allowlistTrackEvents?: Array<string>;
 }
 
 const FULLSTORY_PLUGIN_CONFIG_DEFAULTS: FullStoryPluginConfig = {
   enableFSSessionUrlInEvents: true,
+  allowlistAllTrackEvents: false,
 };
 
 export class FullStoryPlugin extends Plugin {
   public enableFSSessionURLInEvents;
+  public allowlistAllTrackEvents;
+  public allowlistTrackEvents;
+
   private fsSessionUrl = '';
 
   type = PluginType.enrichment;
@@ -31,18 +37,25 @@ export class FullStoryPlugin extends Plugin {
     const config = { ...FULLSTORY_PLUGIN_CONFIG_DEFAULTS, ...userConfig };
 
     this.enableFSSessionURLInEvents = config.enableFSSessionUrlInEvents;
+    this.allowlistAllTrackEvents = config.allowlistAllTrackEvents;
+    this.allowlistTrackEvents = config.allowlistTrackEvents;
   }
 
   execute(event: SegmentEvent) {
     switch (event.type) {
       case 'track':
-        const fsProps = new FSSuffixedProperties(event.properties);
-        FullStory.event(event.event, fsProps.getSuffixedProperties());
+        if (
+          this.allowlistAllTrackEvents ||
+          (this.allowlistTrackEvents &&
+            this.allowlistTrackEvents.includes(event.event))
+        ) {
+          const fsProps = new FSSuffixedProperties(event.properties);
+          FullStory.event(event.event, fsProps.getSuffixedProperties());
 
-        if (this.enableFSSessionURLInEvents && this.fsSessionUrl) {
-          this.addFSUrlToProperties(event as TrackEventType);
+          if (this.enableFSSessionURLInEvents && this.fsSessionUrl) {
+            this.addFSUrlToProperties(event as TrackEventType);
+          }
         }
-
         break;
       case 'identify':
         break;
--- src/__tests__/FullStoryConfig.test.ts
+++ src/__tests__/FullStoryConfig.test.ts
@@ -1,9 +1,12 @@
 import { FullStoryPlugin } from '../FullStoryConfig';
 import { EventType, TrackEventType } from '@segment/analytics-react-native';
+import FullStory from '@fullstory/react-native';
+
+const EVENT_NAME = 'sample event';
 
 const TRACK_EVENT: TrackEventType = {
   type: EventType.TrackEvent,
-  event: 'sample event',
+  event: EVENT_NAME,
 };
 
 interface TrackEventTypeWIthFSUrl extends TrackEventType {
@@ -13,9 +16,16 @@ interface TrackEventTypeWIthFSUrl extends TrackEventType {
 }
 
 describe('FullStoryConfig', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   describe('Test enableFSSessionURLInEvents', () => {
     test('Should have FSUrl with enableFSSessionURLInEvents: true', async () => {
-      const plugin = new FullStoryPlugin({ enableFSSessionUrlInEvents: true });
+      const plugin = new FullStoryPlugin({
+        enableFSSessionUrlInEvents: true,
+        allowlistAllTrackEvents: true,
+      });
 
       await new Promise(process.nextTick);
 
@@ -27,13 +37,55 @@ describe('FullStoryConfig', () => {
     });
 
     test('Should not have FSUrl with enableFSSessionURLInEvents: false', async () => {
-      const plugin = new FullStoryPlugin({ enableFSSessionUrlInEvents: false });
+      const plugin = new FullStoryPlugin({
+        enableFSSessionUrlInEvents: false,
+        allowlistAllTrackEvents: true,
+      });
 
       await new Promise(process.nextTick);
 
       const event = plugin.execute({ ...TRACK_EVENT }) as TrackEventType;
 
       expect(event.properties).toBe(undefined);
+    });
+  });
+
+  describe('Test allowlistTrackEvents', () => {
+    test('Should not send track events', () => {
+      const plugin = new FullStoryPlugin({
+        allowlistAllTrackEvents: false,
+      });
+
+      plugin.execute({
+        ...TRACK_EVENT,
+      }) as TrackEventTypeWIthFSUrl;
+
+      expect(FullStory.event).not.toHaveBeenCalled();
+    });
+
+    test('Should send track events with allowlistAllTrackEvents', () => {
+      const plugin = new FullStoryPlugin({
+        allowlistAllTrackEvents: true,
+      });
+
+      plugin.execute({
+        ...TRACK_EVENT,
+      }) as TrackEventTypeWIthFSUrl;
+
+      expect(FullStory.event).toHaveBeenCalled();
+    });
+
+    test('Should send track events with allowlistTrackEvents', () => {
+      const plugin = new FullStoryPlugin({
+        allowlistAllTrackEvents: false,
+        allowlistTrackEvents: [EVENT_NAME],
+      });
+
+      plugin.execute({
+        ...TRACK_EVENT,
+      }) as TrackEventTypeWIthFSUrl;
+
+      expect(FullStory.event).toHaveBeenCalled();
     });
   });
 });
